1b243a7f8da0f3d5c4482eb506719eaad3c621a0
anderson.bockor <anderson.bockor@senior.com.br>
2018-09-23 22:35:23 -0300

    no message

---
+++ kafka-consumer/src/main/java/kafka_consumer/kafka_consumer/Consumer.java
@@ -0,0 +1,95 @@
+package kafka_consumer.kafka_consumer;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import com.google.gson.Gson;
+
+class MsgKafka 
+{
+    	
+	private String id;
+	private String timestamp;
+	private String data;
+    
+	public String getId() {
+		return id;
+	}
+  
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getTimestamp() {
+		return timestamp;
+	}
+	
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}	
+	
+	public String getData() {
+		return data;
+	}
+	
+	public void setData(String data) {
+		this.data = data;
+	}
+
+}
+
+public class Consumer 
+{
+	public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
+		
+		Properties properties = new Properties();
+		properties.put("bootstrap.servers", "localhost:9092");
+		properties.put("compression.type" , "gzip");
+		properties.put("key.deserializer"   , "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put("value.deserializer" , "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put("max.partition.fetch.bytes", "2097152");
+		properties.put("max.poll.records"          , "500");
+		properties.put("group.id"          , "my-group");
+		
+		runMainLoop(args, properties);
+	}
+	
+	static void runMainLoop(String[] args, Properties properties) throws InterruptedException, UnsupportedEncodingException {
+		
+		// Create Kafka comsumer
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+		
+		try {
+			//Exemplo lendo 2 bombas
+			consumer.subscribe(Arrays.asList("Bomba1","Bomba2"));
+  	      	
+			System.out.println("Subscribed to topic Bomba1 and Bomba2");	
+  	      	
+			while (true) 
+			{
+  	      		ConsumerRecords<String, String> records = consumer.poll(100);
+  	      		
+  	      		for (ConsumerRecord<String, String> record : records){
+  	            	System.out.printf("partition = %s, offset = %d, key = %s, value = %s\n", record.partition(), record.offset(), record.key() ,decodeMsg(record.value()).getId());
+  	      		}
+			}
+		}
+		finally {
+			consumer.close();
+		}
+	}
+	
+	public static MsgKafka decodeMsg(String json) throws UnsupportedEncodingException {
+        Gson gson = new Gson();
+        MsgKafka msg = gson.fromJson(json, MsgKafka.class); 
+        byte[] encodedData = Base64.getDecoder().decode(msg.getData()); 
+		msg.setData(new String(encodedData, "utf-8"));
+		return msg;		
+	}
+}
